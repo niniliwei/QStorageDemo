0630298dd673ff8ba8a5b231190c6f4b510a0e66
liwei <liwei@uama.com.cn>
2019-06-03 15:53:42 +0800

    添加注释并调整方法位置

--- app/src/main/java/com/niniliwei/qstoragedemo/MainActivity.kt
+++ app/src/main/java/com/niniliwei/qstoragedemo/MainActivity.kt
@@ -16,7 +16,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.os.BuildCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
@@ -38,7 +37,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        contributingFilesButton.setOnClickListener {
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 向 MediaStore.Images 中写入文件
+        writeFileToMediaImagesButton.setOnClickListener {
             val values = ContentValues().apply {
                 put(MediaStore.Images.Media.DISPLAY_NAME, "IMG1024.JPG")
                 put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
@@ -66,22 +67,25 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 指定相对路径和设备名称
         customRelativePathAndVolumeButton.setOnClickListener {
-            if (BuildCompat.isAtLeastQ()) {
-                val volumeNames = MediaStore.getAllVolumeNames(this)
-                        .filter { it != "internal" }
-                        .toTypedArray()
-                AlertDialog.Builder(this)
-                        .setTitle("请选择卷名")
-                        .setItems(volumeNames) { _, which ->
-                            insertMp3File(volumeNames[which])
-                        }
-                        .show()
-            }
+            val volumeNames = MediaStore.getAllVolumeNames(this)
+                    .filter { it != "internal" }
+                    .toTypedArray()
+            AlertDialog.Builder(this)
+                    .setTitle("请选择卷名")
+                    .setItems(volumeNames) { _, which ->
+                        writeMp3FileToMusic(volumeNames[which])
+                    }
+                    .show()
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 不申请权限读取所有图片
         readImagesWithoutPermissionButton.setOnClickListener {
             readAllImages()
         }
+        // 申请权限读取所有图片
         readImagesWithPermissionButton.setOnClickListener {
             if (ActivityCompat.checkSelfPermission(
                             this,
@@ -96,6 +100,8 @@ class MainActivity : AppCompatActivity() {
                 readAllImages()
             }
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 向 MediaStore.Downloads 中写入文件
         writeFileToMediaDownloadsButton.setOnClickListener {
             val values = ContentValues().apply {
                 put(MediaStore.Images.Media.DISPLAY_NAME, "IMG1024_Downloads.JPG")
@@ -123,9 +129,11 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "操作失败", Toast.LENGTH_SHORT).show()
             }
         }
+        // 不申请权限读取 MediaStore.Downloads 中所有文件
         readDownloadsWithoutPermissionButton.setOnClickListener {
             readDownloadsFiles()
         }
+        // 申请权限读取 MediaStore.Downloads 中所有文件
         readDownloadsWithPermissionButton.setOnClickListener {
             if (ActivityCompat.checkSelfPermission(
                             this,
@@ -140,9 +148,12 @@ class MainActivity : AppCompatActivity() {
                 readDownloadsFiles()
             }
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 不申请权限删除所有图片中的第一张
         deleteFirstImageWithoutPermissionButton.setOnClickListener {
             deleteFirstImage()
         }
+        // 申请权限删除所有图片中的第一张
         deleteFirstImageWithPermissionButton.setOnClickListener {
             if (ActivityCompat.checkSelfPermission(
                             this,
@@ -157,6 +168,8 @@ class MainActivity : AppCompatActivity() {
                 deleteFirstImage()
             }
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 向 SD 卡写入文件
         writeFileToSDCardButton.setOnClickListener {
             if (ActivityCompat.checkSelfPermission(
                             this,
@@ -171,6 +184,8 @@ class MainActivity : AppCompatActivity() {
                 writeFileToSDCard()
             }
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // 通过 SAF 打开文档
         safOpenDocumentButton.setOnClickListener {
             Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
@@ -179,6 +194,7 @@ class MainActivity : AppCompatActivity() {
                 startActivityForResult(this, REQUEST_CODE_OPEN_DOCUMENT)
             }
         }
+        // 通过 SAF 创建文档
         safCreateDocumentButton.setOnClickListener {
             Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
                 addCategory(Intent.CATEGORY_OPENABLE)
@@ -216,35 +232,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun readDownloadsFiles() {
-        val collection = MediaStore.Downloads.EXTERNAL_CONTENT_URI
-        val cursor = contentResolver.query(collection, null, null, null, null) ?: return
-
-        cursor.use {
-            Log.i(TAG, "downloads cursor size: ${cursor.count}")
-            while (cursor.moveToNext()) {
-                val item = ContentUris.withAppendedId(
-                        collection,
-                        cursor.getLong(cursor.getColumnIndex(MediaStore.Downloads._ID))
-                )
-                val displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME))
-                val mimeType = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.MIME_TYPE))
-                Log.i(TAG, "downloads item: $item(displayName: $displayName, mimeType:$mimeType)")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == REQUEST_PERMISSION_READ_EXTERNAL_STORAGE_FOR_IMAGES) {
+                readAllImages()
+            } else if (requestCode == REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE_FOR_DELETING_FIRST_IMAGE) {
+                deleteFirstImage()
+            } else if (requestCode == REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE_FOR_WRITING_TO_SDCARD) {
+                writeFileToSDCard()
             }
+        } else {
+            Log.i(TAG, "PERMISSION REQUEST DENIED!!!")
         }
     }
 
-    private fun writeFileToSDCard() {
-        val dir = Environment.getExternalStoragePublicDirectory("MyPictures")
-        if (!dir.exists()) dir.mkdirs()
+    /**
+     * 向 Music 中写入一个 mp3 文件，并制定设备和相对路径
+     */
+    private fun writeMp3FileToMusic(volumeName: String) {
+        val values = ContentValues().apply {
+            put(MediaStore.Audio.Media.RELATIVE_PATH,
+                    "Music/My Artist/My Album")
+            put(MediaStore.Audio.Media.DISPLAY_NAME,
+                    "My Song.mp3")
+            put(MediaStore.Audio.Media.IS_PENDING, 1)
+        }
 
-        val file = File(dir, "IMG1024.JPG")
+        val collection = MediaStore.Audio.Media.getContentUri(volumeName)
+        val item = contentResolver.insert(collection, values) ?: return
+
         try {
-            resources.assets.open("tim-meyer.jpg").use { inputStream ->
-                FileOutputStream(file).use { outputStream ->
+            contentResolver.openOutputStream(item)?.use { outputStream ->
+                resources.assets.open("libai.mp3").use { inputStream ->
                     inputStream.copyTo(outputStream)
                 }
             }
+
+            values.clear()
+            values.put(MediaStore.Audio.Media.IS_PENDING, 0)
+            contentResolver.update(item, values, null, null)
 
             Toast.makeText(this, "操作成功", Toast.LENGTH_SHORT).show()
         } catch (e: IOException) {
@@ -253,6 +280,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 读取所有图片
+     */
+    private fun readAllImages() {
+        val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        // val collectionWithPending = MediaStore.setIncludePending(collection)
+        val cursor = contentResolver.query(collection, null, null, null, null) ?: return
+
+        cursor.use {
+            Log.i(TAG, "images cursor size: ${cursor.count}")
+            while (cursor.moveToNext()) {
+                val imageUri = ContentUris.withAppendedId(
+                        collection,
+                        cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media._ID))
+                )
+                val displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME))
+                val mimeType = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.MIME_TYPE))
+                Log.i(TAG, "imageUri: $imageUri(displayName: $displayName, mimeType:$mimeType)")
+            }
+        }
+    }
+
+    /**
+     * 删除第一张图片
+     */
     private fun deleteFirstImage() {
         val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val cursor = contentResolver.query(collection, null, null, null, null)
@@ -284,67 +336,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (requestCode == REQUEST_PERMISSION_READ_EXTERNAL_STORAGE_FOR_IMAGES) {
-                readAllImages()
-            } else if (requestCode == REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE_FOR_DELETING_FIRST_IMAGE) {
-                deleteFirstImage()
-            } else if (requestCode == REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE_FOR_WRITING_TO_SDCARD) {
-                writeFileToSDCard()
-            }
-        } else {
-            Log.i(TAG, "PERMISSION REQUEST DENIED!!!")
-        }
-    }
+    /**
+     * 向 SD 卡中写入文件
+     */
+    private fun writeFileToSDCard() {
+        val dir = Environment.getExternalStoragePublicDirectory("MyPictures")
+        if (!dir.exists()) dir.mkdirs()
 
-    private fun readAllImages() {
-        val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        // val collectionWithPending = MediaStore.setIncludePending(collection)
-        val cursor = contentResolver.query(collection, null, null, null, null) ?: return
-
-        cursor.use {
-            Log.i(TAG, "images cursor size: ${cursor.count}")
-            while (cursor.moveToNext()) {
-                val imageUri = ContentUris.withAppendedId(
-                        collection,
-                        cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media._ID))
-                )
-                val displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME))
-                val mimeType = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.MIME_TYPE))
-                Log.i(TAG, "imageUri: $imageUri(displayName: $displayName, mimeType:$mimeType)")
-            }
-        }
-    }
-
-    private fun insertMp3File(volumeName: String) {
-        val values = ContentValues().apply {
-            put(MediaStore.Audio.Media.RELATIVE_PATH,
-                    "Music/My Artist/My Album")
-            put(MediaStore.Audio.Media.DISPLAY_NAME,
-                    "My Song.mp3")
-            put(MediaStore.Audio.Media.IS_PENDING, 1)
-        }
-
-        val collection = MediaStore.Audio.Media.getContentUri(volumeName)
-        val item = contentResolver.insert(collection, values) ?: return
-
+        val file = File(dir, "IMG1024.JPG")
         try {
-            contentResolver.openOutputStream(item)?.use { outputStream ->
-                resources.assets.open("libai.mp3").use { inputStream ->
+            resources.assets.open("tim-meyer.jpg").use { inputStream ->
+                FileOutputStream(file).use { outputStream ->
                     inputStream.copyTo(outputStream)
                 }
             }
-
-            values.clear()
-            values.put(MediaStore.Audio.Media.IS_PENDING, 0)
-            contentResolver.update(item, values, null, null)
 
             Toast.makeText(this, "操作成功", Toast.LENGTH_SHORT).show()
         } catch (e: IOException) {
             e.printStackTrace()
             Toast.makeText(this, "操作失败", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * 读取 MediaStorage.Downloads 下所有文件
+     */
+    private fun readDownloadsFiles() {
+        val collection = MediaStore.Downloads.EXTERNAL_CONTENT_URI
+        val cursor = contentResolver.query(collection, null, null, null, null) ?: return
+
+        cursor.use {
+            Log.i(TAG, "downloads cursor size: ${cursor.count}")
+            while (cursor.moveToNext()) {
+                val item = ContentUris.withAppendedId(
+                        collection,
+                        cursor.getLong(cursor.getColumnIndex(MediaStore.Downloads._ID))
+                )
+                val displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME))
+                val mimeType = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.MIME_TYPE))
+                Log.i(TAG, "downloads item: $item(displayName: $displayName, mimeType:$mimeType)")
+            }
         }
     }
 }
